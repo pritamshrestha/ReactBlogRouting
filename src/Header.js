71544f15260a1f4f010b99d5a4285205ef6ca4a3
pritam shrestha <safal_pritam@hotmail.com>
2017-06-28 09:21:24 -0700

    first commit

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+  render(){
+    return(
+      <div>
+        <header className="intro-header pritam">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                        <div className="site-heading">
+                            <h1>Clean Blog</h1>
+                            <hr className="small" />
+                            <span className="subheading">A Clean Blog Theme by Start Bootstrap</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+      </div>
+    );
+  }
+}
+export default Header;
